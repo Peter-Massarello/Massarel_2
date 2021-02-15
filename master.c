3f9b775809bf4f6b447e1f08a0d37cfabae7d963
Peter-Massarello <pmassarello@gmail.com>
2021-02-15 12:27:00 -0600

    Added help_menu placeholder

--- master.c
+++ master.c
@@ -6,8 +6,31 @@
 #include <errno.h>
 
 
+void help_menu() {
+	printf("HELP MENU TO BE ADDED HERE\n\n");
+}
+
+
+
+
 int main(int argc, char* argv[]){
 
-	printf("Hello World!\n");
+	int opt;
+	if (argc == 1)
+	{
+		system("clear");
+		printf("Program called with no arguments, use ./master -h for help\n");
+		return 0;
+	}
+	while((opt = getopt(argc, argv, "h")) != -1)
+	{
+		system("clear");
+		switch(opt)
+		{
+			case 'h':
+				help_menu();
+				break;
+		}
+	}
 	return 0;
 }
