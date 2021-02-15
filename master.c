188ee20c45b88df725d63fe82af5c0625caa978f
Peter-Massarello <pmassarello@gmail.com>
2021-02-15 12:07:26 -0600

    First Commit of Project 2

---
+++ master.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <stdbool.h>
+#include <errno.h>
+
+
+int main(int argc, char* argv[]){
+
+	printf("Hello World!\n");
+	return 0;
+}
